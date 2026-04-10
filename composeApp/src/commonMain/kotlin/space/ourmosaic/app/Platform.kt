53ecdb65bf778381da36845ff34eb13ab27c7222
chmod 777 <florentin.martorell@etu.univ-grenoble-alpes.fr>
2026-04-10 18:15:18 +0200

    initial commit

---
+++ composeApp/src/commonMain/kotlin/space/ourmosaic/app/Platform.kt
@@ -0,0 +1,7 @@
+package space.ourmosaic.app
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
---
+++ composeApp/src/iosMain/kotlin/space/ourmosaic/app/MainViewController.kt
@@ -0,0 +1,5 @@
+package space.ourmosaic.app
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
