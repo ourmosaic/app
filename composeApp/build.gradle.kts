a32c8eb3f9dd9838700013218fdf3d62ce55dccd
chmod 777 <florentin.martorell@etu.univ-grenoble-alpes.fr>
2026-05-26 19:46:43 +0200

    release: 0.0.11

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -89,7 +89,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 5
-        versionName = "0.0.1"
+        versionName = "0.0.11"
     }
     packaging {
         resources {
